142d2919dad5ea836652f86d3cab490fba67648e
Aditya Gupta <aditya3492gupta@gmail.com>
2025-05-28 09:48:19 +0530

    dto added

--- src/main/java/com/abes/lms/dto/UserDTO.java
+++ src/main/java/com/abes/lms/dto/UserDTO.java
@@ -1,4 +1,38 @@
 package com.abes.lms.dto;
 
 public class UserDTO {
+    private String name;
+    private String password;
+    private String id;
+
+    // constructor for the user DTO
+    public UserDTO(String name, String password, String id) {
+        setName(name);
+        setPassword(password);
+        setId(id);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
