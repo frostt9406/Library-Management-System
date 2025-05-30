2a16571a90dff74d67cb6a57e3224dc9782e934f
vishwajeet-rajputt <vishwajeetchhonker@gmail.com>
2025-05-30 10:50:36 +0530

    DAO Impl Added

--- src/main/java/com/abes/lms/dao/implementation/BookDaoImpl.java
+++ src/main/java/com/abes/lms/dao/implementation/BookDaoImpl.java
@@ -1,4 +1,53 @@
 package com.abes.lms.dao.implementation;
+import com.abes.lms.dao.BookDAO;
+import com.abes.lms.dto.BookDTO;
+import com.abes.lms.util.CollectionUtil;
 
-public class BookDaoImpl {
+import java.util.List;
+import java.util.Optional;
+public class BookDaoImpl implements BookDAO{
+    private final List<BookDTO> books = CollectionUtil.getBookList();
+
+    @Override
+    public boolean addBook(BookDTO book) {
+        if (isBookPresent(book.getTitle()) || getBookById(book.getId()) != null) {
+            return false;
+        }
+        return books.add(book);
+    }
+
+    @Override
+    public boolean removeBook(String title) {
+        Optional<BookDTO> bookToRemove = books.stream()
+                .filter(book -> book.getTitle().equalsIgnoreCase(title))
+                .findFirst();
+        return bookToRemove.map(books::remove).orElse(false);
+    }
+
+    @Override
+    public boolean isBookPresent(String title) {
+        return books.stream()
+                .anyMatch(book -> book.getTitle().equalsIgnoreCase(title));
+    }
+
+    @Override
+    public List<BookDTO> getAllBooks() {
+        return books;
+    }
+
+    @Override
+    public BookDTO getBookByTitle(String title) {
+        return books.stream()
+                .filter(book -> book.getTitle().equalsIgnoreCase(title))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public BookDTO getBookById(int id) {
+        return books.stream()
+                .filter(book -> book.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
 }
--- src/main/java/com/abes/lms/util/CollectionUtil.java
+++ src/main/java/com/abes/lms/util/CollectionUtil.java
@@ -1,14 +1,17 @@
 package com.abes.lms.util;
 import com.abes.lms.dto.BookDTO;
+import com.abes.lms.dto.LibrarianDTO;
 import com.abes.lms.dto.UserDTO;
 import java.util.*;
 
 public class CollectionUtil {
     private static List<BookDTO> books = new ArrayList<>();
     private static List<UserDTO> users = new ArrayList<>();
+    private static List<LibrarianDTO> libarian = new ArrayList<>();
 
     public static List<BookDTO> getBookList() { return books; }
     public static List<UserDTO> getUserList() { return users; }
+    public static List<LibrarianDTO> getLibarianList() { return libarian; }
 }
 
 
