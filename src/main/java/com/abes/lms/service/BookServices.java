d68d5666832ff22d4fec0b96291e1225760c7d3b
Aryan Choudhary <choudharyaryan2003@gmail.com>
2025-05-30 11:43:58 +0530

    interface code

--- src/main/java/com/abes/lms/service/BookServices.java
+++ src/main/java/com/abes/lms/service/BookServices.java
@@ -1,4 +1,16 @@
 package com.abes.lms.service;
 
-public class BookServices {
+import com.abes.lms.dto.BookDTO;
+import java.util.List;
+
+public interface BookServices {
+    boolean addBook(BookDTO book);
+    boolean removeBook(String title);
+    boolean isBookPresent(String title);
+    List<BookDTO> getAllBooks();
+    BookDTO getBookByTitle(String title);
+    BookDTO getBookById(int id);
+    List<BookDTO> sortBooksById();
+    List<BookDTO> sortBooksByRating();
+    List<BookDTO> sortBooksByTitle();
 }
--- src/main/java/com/abes/lms/service/UserServices.java
+++ src/main/java/com/abes/lms/service/UserServices.java
@@ -1,4 +1,12 @@
 package com.abes.lms.service;
+import com.abes.lms.dto.BookDTO;
+import java.util.List;
 
-public class UserServices {
+public interface UserServices {
+    void borrowBook(String username, String title);
+    void returnBook(String username, String title);
+    List<BookDTO> viewBooks();
+    List<BookDTO> sortBooksById();
+    List<BookDTO> sortBooksByRating();
+    List<BookDTO> sortBooksByTitle();
 }
